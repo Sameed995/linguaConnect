25688b5f587eec12bdc06629ce67af97e30a606e
Sameed995 <sameedshaikh346@gmail.com>
2026-06-17 12:02:48 +0530

    basic toast notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,22 @@ import OnboardingPage from './pages/OnboardingPage.jsx';
 import NotificationsPage from './pages/NotificationsPage.jsx';
 import CallPage from './pages/CallPage.jsx';
 import ChatPage from './pages/ChatPage.jsx';
+import toast,{ Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
 <div className=" h-screen" data-theme="dark">
+    <button onClick={() => toast.success("This is a success message!")}>Create Toast</button>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
-      <Route path="/notifications" element={<NotificationsPage />} />
+      <Route path="/notifications" element={<NotificationsPage   />} />
       <Route path="/call" element={<CallPage />} />
       <Route path="/chat" element={<ChatPage />} />
     </Routes>
+    <Toaster />
 </div>
   )
 }
