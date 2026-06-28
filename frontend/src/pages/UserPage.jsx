35b4351b27380302cb4331824e5c55251c1bd522
Sameed995 <sameedshaikh346@gmail.com>
2026-06-28 16:01:51 +0530

    created UserPage.jsx

---
+++ frontend/src/pages/UserPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>this is the userpage, where users can view their profile and settings</div>
+  )
+}
+
+export default UserPage
